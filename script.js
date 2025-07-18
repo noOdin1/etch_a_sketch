2e964c359c08ec60be9f34c798bed3cbd64faa4e
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-19 05:41:27 +0800

    Added all the appropriate divs

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 function createDiv() {
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-      // here is where we create the divs
+  let mainDiv = document.querySelector(".main.content");
+
+  // Strategy:
+  //  Create 16 rows of div containers that contain 16 cells
+  //  of divs align in a row
+  for (let i = 0; i < drawingSize; i++) {
+    // here is where we create the divs
+    var tmpRowDiv = document.createElement("div");
+    tmpRowDiv.classList.add("row_" + i);
+    tmpRowDiv.classList.add("row");
+    mainDiv.appendChild(tmpRowDiv);
+    for (let j = 0; j < drawingSize; j++) {
+      var tmpColDiv = document.createElement("div");
+      tmpColDiv.classList.add("col_" + j);
+      tmpColDiv.classList.add("col");
+      tmpRowDiv.appendChild(tmpColDiv);
     }
   }
 }
+
+const drawingSize = 16;
+createDiv();
