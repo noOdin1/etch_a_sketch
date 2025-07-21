dd066b0ad3c9216394c6c6fea241ce508bd519c6
Robin Mowe <218098565+noOdin1@users.noreply.github.com>
2025-07-21 18:31:53 +0800

    Reset for the brightness filter

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ function easMouseOver(event) {
     if (event.target.classList.contains("random")) {
       event.target.classList.remove("random");
       event.target.style.backgroundColor = "";
+      event.target.style.filter = `brightness(100%)`; // reset the brightness filter
+      event.target.dataset.brightness = "100";
     }
   }
 }
